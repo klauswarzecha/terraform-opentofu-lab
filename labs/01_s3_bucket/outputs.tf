86ca425380291597f31863f39ba78ffbba8cdb14
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-06 21:04:13 +0100

    feat: Add first lab (01_s3_bucket) (#5)

---
+++ labs/01_s3_bucket/outputs.tf
@@ -0,0 +1,4 @@
+output "bucket_name" {
+  value       = module.bucket.bucket_name
+  description = "Name of the lab bucket."
+}
---
+++ labs/01_s3_bucket/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  type        = string
+  description = "AWS region."
+  default     = "eu-central-1"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Project name."
+  default     = "terraform-opentofu-lab"
+}
