44672c09669de009b796739adf33b43bd93a9038
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-07 14:52:02 +0100

    feat: Add second lab (02_vpc)

---
+++ labs/02_vpc_light/main.tf
@@ -0,0 +1,7 @@
+module "network" {
+  source              = "../../modules/network_light"
+  name_prefix         = "${var.project_name}-02"
+  vpc_cidr            = "10.10.0.0/16"
+  public_subnet_cidrs = ["10.10.1.0/24", "10.10.2.0/24"]
+  tags                = var.tags
+}
