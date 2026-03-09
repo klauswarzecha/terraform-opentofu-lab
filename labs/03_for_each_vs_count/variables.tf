706a1ec7097be7e622dc42953099601c0c680eaa
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-09 12:23:25 +0100

    Feature/lab 03 for each vs count
    * feat: Add lab 03 demonstrating count vs for_each
    
    * refactor: Switch lab 03 resources from count to for_each

--- labs/03_for_each_vs_count/variables.tf
+++ labs/03_for_each_vs_count/variables.tf
@@ -4,11 +4,12 @@ variable "aws_region" {
 }
 
 variable "subnet_cidrs" {
-  description = "Two subnet CIDR blocks"
-  type        = list(string)
+  description = "Subnet CIDR blocks keyed by logical subnet name."
+  type        = map(string)
 
-  default = [
-    "10.20.1.0/24",
-    "10.20.2.0/24"
-  ]
+  default = {
+    public_x        = "10.20.99.0/24"
+    public_subnet_a = "10.20.1.0/24"
+    public_subnet_b = "10.20.2.0/24"
+  }
 }
