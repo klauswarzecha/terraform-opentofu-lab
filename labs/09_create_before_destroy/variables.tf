d250e4e27ab1d617c13d4370253e39fa94711be2
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-15 19:00:18 +0100

    Feature/lab 09 create before destroy (#15)
    * Add variables.tf
    
    * Add versions.tf
    
    * Add providers.tf
    
    * Add outputs.tf
    
    * Add main.tf
    
    * Add main.tf
    
    * Modify name of security group
    
    * trigger replacement of sg

---
+++ labs/09_create_before_destroy/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region for the lab."
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "sg_name" {
+  description = "Security group name."
+  type        = string
+  default     = "lab-09-sg-renamed"
+}
