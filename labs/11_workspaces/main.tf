f1c4edc21289a012a6d35deefa60374d79390334
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-17 21:12:59 +0100

    fix: Change main.tf

--- labs/11_workspaces/main.tf
+++ labs/11_workspaces/main.tf
@@ -1,7 +1,16 @@
-output "workspace" {
-  value = terraform.workspace
+resource "random_id" "suffix" {
+  byte_length = 2
 }
 
-output "bucket_name" {
-  value = aws_s3_bucket.workspace_bucket.bucket
+locals {
+  bucket_name = "lab-11-${terraform.workspace}-${random_id.suffix.hex}"
+}
+
+resource "aws_s3_bucket" "workspace_bucket" {
+  bucket = local.bucket_name
+
+  tags = {
+    Workspace = terraform.workspace
+    Lab       = "11"
+  }
 }
