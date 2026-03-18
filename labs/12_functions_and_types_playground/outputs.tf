2ea15409f4048a9e8b576f8777664f1242fc0ecb
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-18 20:49:09 +0100

    Add playground with functions (#18)

---
+++ labs/12_functions_and_types_playground/outputs.tf
@@ -0,0 +1,44 @@
+output "upper_names" {
+  value = local.upper_names
+}
+
+output "distinct_names" {
+  value = local.distinct_names
+}
+
+output "names_as_set" {
+  value = local.names_as_set
+}
+
+output "names_as_list_again" {
+  value = local.names_as_list_again
+}
+
+output "merged_labels" {
+  value = local.merged_labels
+}
+
+output "label_keys" {
+  value = local.label_keys
+}
+
+output "label_values" {
+  value = local.label_values
+}
+
+output "name_lengths" {
+  value = local.name_lengths
+}
+
+output "indexed_names" {
+  value = local.indexed_names
+}
+
+output "three_subnet_cidrs" {
+  value = local.three_subnet_cidrs
+}
+
+
+output "flat_list" {
+  value = local.flat_list
+}
