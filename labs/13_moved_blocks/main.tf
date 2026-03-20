6f24970040a9e52522838b06433726dac9ac1685
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-20 17:00:09 +0100

    Adjust naming of the resource

--- labs/13_moved_blocks/main.tf
+++ labs/13_moved_blocks/main.tf
@@ -1,10 +1,19 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "core" {
   cidr_block           = "10.130.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
 
+  lifecycle {
+    prevent_destroy = true
+  }
+
   tags = {
     Name = "lab-13-vpc"
-    Lab  = 13
+    Lab  = "13"
   }
+}
+
+moved {
+  from = aws_vpc.main
+  to   = aws_vpc.core
 }
--- labs/13_moved_blocks/outputs.tf
+++ labs/13_moved_blocks/outputs.tf
@@ -1,4 +1,4 @@
 output "vpc_id" {
   description = "ID of the  VPC."
-  value       = aws_vpc.main.id
+  value       = aws_vpc.core.id
 }
