2656dce0d309004b92e51d375ce024b163c91eb5
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-21 22:05:52 +0100

    feat: Add lab 12b for defensive Terraform functions (try, can, lookup, coalesce) (#21)

---
+++ labs/12b_defensive_functions/outputs.tf
@@ -0,0 +1,23 @@
+output "env" {
+  value = local.env
+}
+
+output "description" {
+  value = local.description
+}
+
+output "has_owner" {
+  value = local.has_owner
+}
+
+output "owner" {
+  value = local.owner
+}
+
+output "name" {
+  value = local.name
+}
+
+output "nested_try" {
+  value = local.nested_try
+}
