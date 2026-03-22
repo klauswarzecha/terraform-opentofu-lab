d1c3ec1eaf23b7aa9b6745b74a51c2131d2743b2
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-22 20:13:16 +0100

    Add lab 14 for state manipulation (#22)

---
+++ labs/14_state_cli/main.tf
@@ -0,0 +1,10 @@
+resource "aws_vpc" "lab" {
+  cidr_block           = "10.140.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "lab-14-vpc"
+    Lab  = "14"
+  }
+}
