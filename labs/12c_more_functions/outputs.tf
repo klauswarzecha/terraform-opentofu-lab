835e681de221d37dd20a8cf8b75adae8e03ea3bc
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-27 20:36:33 +0100

    Feature/lab 12c more functions (#27)
    * Use a simple map
    
    The simple map(strings) only provides names for creating S3 buckets via for_each.
    
    * Examine functions and for loops

---
+++ labs/12c_more_functions/outputs.tf
@@ -0,0 +1,25 @@
+output "flattened_list" {
+  value = local.numbers
+}
+
+output "combined_dicts" {
+  value = local.combined
+}
+
+output "sloppy_fruits" {
+  value = var.sloppy_fruits
+}
+
+output "stripped_fruits" {
+  value = local.stripped_fruits
+}
+
+output "title_fruits" {
+  value = local.title_fruits
+}
+
+output "fruit_map" {
+  value = local.fruit_map
+}
+
+
