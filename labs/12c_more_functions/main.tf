3d5802648aff52c6261d8d9a62873d334da4268f
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-28 23:16:47 +0100

    Add examples for maps and lists

--- labs/12c_more_functions/main.tf
+++ labs/12c_more_functions/main.tf
@@ -15,6 +15,15 @@ locals {
     { Numbers = [34, 45, 67] }
   )
 
+
+
+
+
+}
+
+
+# loop over lists
+locals {
   # strip elements
   stripped_fruits = [
     for fruit in var.sloppy_fruits : trimspace(fruit)
@@ -25,7 +34,29 @@ locals {
     for fruit in local.stripped_fruits : title(fruit)
   ]
 
+  # Print the index and the element
+  indexed_fruits = [
+    for i, fruit in local.stripped_fruits : "${i} - ${fruit}"
+  ]
 
+  # list(dict) makes a lot of sense in Python. But this is Terraform!
+
+  # Ceate a map with the index as the key
+  numbered_fruits = [
+    for i, fruit in local.stripped_fruits : tomap({ "${i}" = "${fruit}" })
+  ]
+
+  # Ceate a map with the fruit as the key
+  weird_fruits = [
+    for i, fruit in local.stripped_fruits : tomap({ "${fruit}" = "${i}" })
+  ]
+}
+
+
+
+
+# loop over maps
+locals {
   fruit_map = {
     for fruit in local.stripped_fruits : fruit => {
       upper  = upper(fruit)
@@ -33,6 +64,11 @@ locals {
     }
   }
 
+  numbers_map = {
+    for num in local.numbers : num => num * num
+  }
 
+  weird_fruits_map = {
+    for i, fruit in local.stripped_fruits : fruit => i
+  }
 }
-
--- labs/12c_more_functions/outputs.tf
+++ labs/12c_more_functions/outputs.tf
@@ -18,8 +18,27 @@ output "title_fruits" {
   value = local.title_fruits
 }
 
+output "indexed_fruits" {
+  value = local.indexed_fruits
+}
+
+output "numbered_fruits" {
+  value = local.numbered_fruits
+}
+
+output "weird_fruits" {
+  value = local.weird_fruits
+}
+
 output "fruit_map" {
   value = local.fruit_map
 }
 
+output "numbers_map" {
+  value = local.numbers_map
+}
 
+
+output "weird_fruits_map" {
+  value = local.weird_fruits_map
+}
