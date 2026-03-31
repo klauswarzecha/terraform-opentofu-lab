b074395c4e3cd384d3c1b50b290790596fd86f39
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-03-31 23:16:16 +0200

    Use map(object) for the description

--- labs/16_for_each_challenge/main.tf
+++ labs/16_for_each_challenge/main.tf
@@ -5,22 +5,26 @@ resource "random_id" "this" {
 
 locals {
   lab_name = "16-for-each-challenge"
-
-  bucket_map = {
-    for label in var.bucket_labels : label => {
-      bucket_name = "${label}-${random_id.this.hex}"
-      tags = merge(
-        var.general_buckets_tags,
-        { Purpose = "Storage of ${label}" },
-        { Lab = "${local.lab_name}" }
-      )
-    }
-  }
 }
 
+#   bucket_map = {
+#     for label in var.bucket_labels : label => {
+#       bucket_name = "${label}-${random_id.this.hex}"
+#       tags = merge(
+#         var.general_buckets_tags,
+#         { Purpose = "Storage of ${label}" },
+#         { Lab = "${local.lab_name}" }
+#       )
+#     }
+#   }
+# }
+
+
+
+
 resource "aws_s3_bucket" "this" {
-  for_each = local.bucket_map
-  bucket   = each.key
+  for_each = var.bucket_definitions
+  bucket   = "${each.key}-${random_id.this.hex}"
   tags     = each.value.tags
 }
 
