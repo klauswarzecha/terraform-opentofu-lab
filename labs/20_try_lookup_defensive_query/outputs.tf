f4b31f0b5d8dd4285bd3021e564e8745cd249026
Klaus-Dieter Warzecha <klauswarzecha@gmail.com>
2026-04-11 23:53:16 +0200

    Checking tags

--- labs/20_try_lookup_defensive_query/outputs.tf
+++ labs/20_try_lookup_defensive_query/outputs.tf
@@ -7,3 +7,11 @@ output "bucket_names" {
     for k, v in aws_s3_bucket.this : k => v.bucket
   }
 }
+
+
+output "bucket_team" {
+  value = {
+    for k, v in aws_s3_bucket.this.tags_all : k => v[Team]
+  }
+}
+
